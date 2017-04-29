896d114807aebfbdfb5ba5fef086886c4568ee25
Nuno Rodrigues <nunorodrigues@Nunos-iMac.lan>
2017-04-29 15:00:57 +0100

    Main structure (Step 1)

--- js/thumbstiles.js
+++ js/thumbstiles.js
@@ -1,1 +1,23 @@
 'use.strict'
+
+var tjsOptions = {
+
+    defaulCols: 4,
+    elementsUrl:"../data/elements.json",
+    eachLoad:20,
+    loadAll:false,
+    responsive:true
+
+}
+
+
+/*
+* Initialize thumbjs
+ */
+function initThumbJs(tjsOptions) {
+
+
+}
+
+
+initThumbJs();
