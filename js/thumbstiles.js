8d3da0a0e0d35ff894f6075491de99a064d2c042
Nuno Rodrigues <nunorodrigues@Nunos-iMac.lan>
2017-04-29 18:22:08 +0100

    testing request

--- js/thumbstiles.js
+++ js/thumbstiles.js
@@ -16,6 +16,19 @@ var tjsOptions = {
  */
 function initThumbJs(tjsOptions) {
 
+var self = this;
+var elementsData = [];
+var requester = new XMLHttpRequest();
+
+self.getElements = function getElements(){
+    requester.open("GET", "../data/elements.json", true);
+    requester.send();
+    requester.onreadystatechange=function() {
+        console.log(this);
+  };
+}
+
+
 
 }
 
